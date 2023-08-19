8e7027c180239424b17bb0dcd1c48825a99f50da
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2023-08-19 15:34:13 +0700

    Implement domain model

---
+++ src/Entity/Author.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AuthorRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AuthorRepository::class)]
+class Author
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 128)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $numberBooks = 0;
+
+    #[ORM\ManyToMany(targetEntity: Book::class, inversedBy: 'authors')]
+    private Collection $books;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+        $this->books = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getNumberBooks(): ?int
+    {
+        return $this->numberBooks;
+    }
+
+    /**
+     * @return Collection<int, Book>
+     */
+    public function getBooks(): Collection
+    {
+        return $this->books;
+    }
+
+    public function addBook(Book $book): static
+    {
+        if (!$this->books->contains($book)) {
+            $this->books->add($book);
+        }
+
+        return $this;
+    }
+
+    public function removeBook(Book $book): static
+    {
+        $this->books->removeElement($book);
+
+        return $this;
+    }
+}
---
+++ src/Entity/Book.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use DateTimeImmutable;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use DomainException;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 1024, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::DATE_IMMUTABLE, nullable: true)]
+    private ?DateTimeImmutable $publicationDate = null;
+
+    #[ORM\ManyToMany(targetEntity: Author::class, mappedBy: 'books')]
+    private Collection $authors;
+
+    public function __construct(
+        string $name,
+        ?string $description,
+        ?DateTimeImmutable $publicationDate,
+        Author ...$authors
+    )
+    {
+        if (empty($authors)) {
+            throw new DomainException('Book must have min 1 author');
+        }
+        $this->name = $name;
+        $this->description = $description;
+        $this->publicationDate = $publicationDate;
+        $this->authors = new ArrayCollection($authors);
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function getPublicationDate(): ?DateTimeImmutable
+    {
+        return $this->publicationDate;
+    }
+
+    /**
+     * @return Collection<int, Author>
+     */
+    public function getAuthors(): Collection
+    {
+        return $this->authors;
+    }
+
+    public function addAuthor(Author $author): static
+    {
+        if (!$this->authors->contains($author)) {
+            $this->authors->add($author);
+            $author->addBook($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAuthor(Author $author): static
+    {
+        if ($this->authors->contains($author) && $this->authors->count() === 1) {
+            throw new DomainException('Book must have min 1 author');
+        }
+        if ($this->authors->removeElement($author)) {
+            $author->removeBook($this);
+        }
+
+        return $this;
+    }
+}
