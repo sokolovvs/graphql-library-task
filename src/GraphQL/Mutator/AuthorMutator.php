d82172ee3edc3dc0f97fd077d26faffc7f260407
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2023-09-01 22:43:14 +0700

    Fix author deletion bug

--- src/GraphQL/Mutator/AuthorMutator.php
+++ src/GraphQL/Mutator/AuthorMutator.php
@@ -68,7 +68,7 @@ final class AuthorMutator
             return new UserError("Unknown author #$id");
         }
         foreach ($author->getBooks() as $book) {
-            if ($book->getAuthors() < 2) {
+            if ($book->getAuthors()->count() < 2) {
                 return new UserError("Can not remove author#{$author->getId()} because book#{$book->getId()} has not another author. Book must has min 1 author");
             }
         }
